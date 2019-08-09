e5f2fc9e564d9cefa29be6679090221f30159678
matejeliash <53951496+matejeliash@users.noreply.github.com>
2019-08-09 21:56:14 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp -r  ~/Downloads/dotfiles/dunst  ~/.config &&  
+cp -r ~/Downloads/dotfiles/i3  ~/.config &&  
+cp -r ~/Downloads/dotfiles/i3blocks  ~/.config &&  
+cp -r ~/Downloads/dotfiles/ranger  ~/.config &&  
+cp -r ~/Downloads/dotfiles/scripts ~/.config  &&  
+cp -r ~/Downloads/dotfiles/urxvt ~/.urxvt &&  
+cp  ~/Downloads/dotfiles/Xresources ~/.Xresources &&  
